0c7c71e58b14e2bf8b57742d79b52b7b49414f35
ferramirezmoriel <96149829+ferramirezmoriel@users.noreply.github.com>
2022-02-25 16:53:50 -0700

    cambios al responsive navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,39 @@
     const nav =
     '<header>\n' +
     '<nav>\n' +
+        '<div class="logo">\n' +
+            '<h4>DAMOR</h4>\n' +
+        '</div>\n' +
+
+
         '<ul class="nav-links">\n' +
-            '<li><img  src="../html/assets/DAMOR_sencillo.png" id="logodamor" width="110" height="110"></img></li>\n' +
-            '<li><a href="Conocenos.html">Conócenos</a></li>\n' +
-            '<li><a href="Desarrolladores.html">Desarrolladores</a></li>\n' +
-            '<li><a href="https://www.animalgourmet.com/2018/02/26/damor-panaderia-guadalajara/">Noticias</a></li>\n' +
-            '<li><a href="Tienda.html">Tienda</a></li>\n' +
-            '<li><a href="https://www.faeeac.org">FAEE</a></li>\n' +
-            '<li><a href="Contacto.html">Contacto</a></li>\n' +
-            '<li><a button href="IniciarSesion.html" id="boton">Iniciar Sesión</a></li>\n' +
-            '<li><a href=""><img src="assets/carrito de compras.png" alt="" id="carrito"></a></li>\n' +
+
+            '<li><a href="../html/Conocenos.html"></a>Conócenos</li>\n' +
+
+            '<li><a href="../html/Tienda.html"></a>Desarrolladores</li>\n' +
+
+            '<li><a href="../html/Tienda.html"></a>Tienda</li>\n' +
+
+            '<li><a href="..https://www.faeeac.org"></a>FAEE</li>\n' +
+
+            '<li><a href="../html/Contacto.html"></a>Contacto</li>\n' +
+
+            '<li><a href="..https://www.animalgourmet.com/2018/02/26/damor-panaderia-guadalajara/"></a>Noticias</li>\n' +
+
+            '<li><a href="../html/IniciarSesion.html"></a>Iniciar Sesion</li>\n' +
+
+            '<li><a href="../html/Conocenos.html"><img src="assets/carrito de compras.png" alt="" id="carrito"></a></li>\n' +
+
         '</ul>\n' +
+        '<div class="burger">\n' +
+            '<div class="line1"></div>\n' +
+            '<div class="line2"></div>\n' +
+            '<div class="line3"></div>\n' +
+        '</div>\n' +
     '</nav>\n' +
+    '<script src="/navbres.js"></script>\n' +
 '</header>';
     const itemsContainer = document.getElementById("navbar");
     itemsContainer.innerHTML += nav;
+
+
