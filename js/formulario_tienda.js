ba79d27df1a54a96c8cbec325b43ddd9dc729501
MrErne0 <ernesto1.diaz2@gmail.com>
2022-03-01 01:56:35 -0600

    paypal

--- js/formulario_tienda.js
+++ js/formulario_tienda.js
@@ -93,6 +93,7 @@ var cart = {
                 // (D3-2) SUBTOTAL
                 subtotal = cart.items[id] * p.price;
                 total += subtotal;
+                
             }
 
             // (D3-3) TOTAL AMOUNT
@@ -104,6 +105,7 @@ var cart = {
 
             // (D3-4) EMPTY & CHECKOUT
             item = document.getElementById("template-cart-checkout").content.cloneNode(true);
+            item.querySelector(".c-checkout").onclick= function (){cart.checkout(total);};
             cart.hItems.appendChild(item);
         }
     },
@@ -142,14 +144,49 @@ var cart = {
     },
 
     // (H) CHECKOUT
-    checkout: () => {
+    checkout: (total) => {
+        
+        item = document.createElement("div");
+            item.className = "paypal-button-container";
+            item.id = "paypal-button-container";
+            cart.hItems.appendChild(item);
+        console.log(total)
+        paypal.Buttons({
+
+            // Sets up the transaction when a payment button is clicked
+            createOrder: function (data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: total // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+                        }
+                    }]
+                });
+            },
+            
+            // Finalize the transaction after payer approval
+            onApprove: function (data, actions) {
+                return actions.order.capture().then(function (orderData) {
+                    // Successful capture! For dev/demo purposes:
+                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                    var transaction = orderData.purchase_units[0].payments.captures[0];
+                    alert('Transaction ' + transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+            
+                    // When ready to go live, remove the alert and show a success message within this page. For example:
+                    // var element = document.getElementById('paypal-button-container');
+                    // element.innerHTML = '';
+                    // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                    // Or go to another URL:  actions.redirect('thank_you.html');
+                });
+            }
+            }).render('#paypal-button-container');   
         // SEND DATA TO SERVER
         // CHECKS
         // SEND AN EMAIL
         // RECORD TO DATABASE
         // PAYMENT
         // WHATEVER IS REQUIRED
-        alert("Confirma compra");
+        //alert("Confirma compra");
 
         /*
         var data = new FormData();
@@ -166,3 +203,4 @@ var cart = {
 };
 window.addEventListener("DOMContentLoaded", cart.init);
 
+        
