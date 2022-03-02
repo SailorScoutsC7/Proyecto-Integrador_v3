6c80c0176a0614c055122e06bd45ca2a5d303888
MiryamHurtado <miryam_hadadelaluna@hotmail.com>
2022-03-01 22:51:31 -0600

    footer

---
+++ js/navbar.js
@@ -0,0 +1,39 @@
+    const nav =
+    '<header>\n' +
+    '<nav>\n' +
+        '<div class="logo">\n' +
+            '<h4>DAMOR</h4>\n' +
+        '</div>\n' +
+
+
+        '<ul class="nav-links">\n' +
+
+            '<li><a href="../html/Conocenos.html">Conócenos</a></li>\n' +
+
+            '<li><a href="../html/Desarrolladores.html">Desarrolladores</a></li>\n' +
+
+            '<li><a href="../html/Tienda2.html">Tienda</a></li>\n' +
+
+            '<li><a href="https://www.faeeac.org">FAEE</a></li>\n' +
+
+            '<li><a href="../html/Contacto.html">Contacto</a></li>\n' +
+
+            '<li><a href="https://www.animalgourmet.com/2018/02/26/damor-panaderia-guadalajara/">Noticias</a></li>\n' +
+
+            '<li><a href="../html/IniciarSesion.html">Iniciar Sesion</a></li>\n' +
+
+            '<li><a id="myBtn" ><img src="assets/carrito de compras.png" alt="" class="carrito"></a></li>\n' +
+
+        '</ul>\n' +
+        '<div class="burger">\n' +
+            '<div class="line1"></div>\n' +
+            '<div class="line2"></div>\n' +
+            '<div class="line3"></div>\n' +
+        '</div>\n' +
+    '</nav>\n' +
+    '<script src="/navbres.js"></script>\n' +
+'</header>';
+    const itemsContainer = document.getElementById("navbar");
+    itemsContainer.innerHTML += nav;
+
+
